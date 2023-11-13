280cdcdc22e17fbaaec30fcaea4ff791185ad8be
mohamedelabbas1996 <hack1996man@gmail.com>
2023-11-13 00:01:15 -0500

    added training loop, cleaned code

---
+++ datasets/utils.py
@@ -0,0 +1,40 @@
+import datetime
+
+
+def map_noaa_to_harps_tarps(harps_noaa, tarps_noaa):
+    df_harps = pd.read_csv(harps_noaa, sep=" ")
+    df_tarps = pd.read_csv(tarps_noaa, sep=" ")
+    noaa_tarps = {}
+    noaa_harps = {}
+    for idx, row in df_harps.iterrows():
+        harp_num = row["HARPNUM"]
+        noaa_num = row["NOAA_ARS"]
+        for ar in noaa_num.split(","):
+            noaa_harps[ar] = harp_num
+    for idx, row in df_tarps.iterrows():
+        tarp_num = row["TARPNUM"]
+        noaa_num = row["NOAA_ARS"]
+        for ar in noaa_num.split(","):
+            noaa_tarps[ar] = harp_num
+    return noaa_harps, noaa_tarps
+
+
+def get_ars(file):
+    lines = open(file).readlines()[1:]
+    ars = []
+    for line in lines:
+        ars.append(line.split()[0])
+    return ars
+
+
+def convert2datetime(datetime_str):
+    return datetime.datetime.strptime(datetime_str[:-4], "%Y-%m-%d %H:%M:%S")
+
+
+def parse_tai_string(tstr):
+    year = int(tstr[:4])
+    month = int(tstr[5:7])
+    day = int(tstr[8:10])
+    hour = int(tstr[11:13])
+    minute = int(tstr[14:16])
+    return datetime.datetime(year, month, day, hour, minute)
---
+++ setup.sh
@@ -0,0 +1,34 @@
+python -m venv solar-flares-forecasting
+source solar-flares-forecasting/bin/activate
+pip install -r requirements.txt
+
+mkdir data
+mkdir data/SHARP
+mkdir data/SHARP/raw_summary_parameters
+mkdir data/SHARP/preprocessed_summary_parameters
+mkdir data/SHARP/summary_parameters_magnetograms
+mkdir data/SHARP/raw_magnetograms
+mkdir data/SHARP/preprocessed_magnetograms
+mkdir data/SHARP/summary_paramters_all_active_regions
+
+mkdir data/SMARP
+mkdir data/SMARP/raw_summary_parameters
+mkdir data/SMARP/preprocessed_summary_parameters
+mkdir data/SMARP/summary_parameters_magnetograms
+mkdir data/SMARP/raw_magnetograms
+mkdir data/SMARP/preprocessed_magnetograms
+mkdir data/SMARP/summary_paramters_all_active_regions
+
+mkdir data/GOES
+wget https://drive.google.com/file/d/1MBG2bsgx3OkJoNfEA63a39bHEWGNWEJ8/view?usp=sharing -O data/GOES/goes.csv
+
+mkdir data/tarp_harp_to_noaa
+wget http://jsoc.stanford.edu/doc/data/hmi/harpnum_to_noaa/all_harps_with_noaa_ars.txt -O data/tarp_harp_to_noaa/harp_noaa.txt
+wget http://jsoc.stanford.edu/doc/data/mdi/all_tarps_with_noaa_ars.txt -O data/tarp_harp_to_noaa/tarp_noaa.txt
+
+
+echo "Downloading data"
+# python datasets/collect_data.py SHARP
+# python datasets/collect_data.py SMARP
+
+echo 
