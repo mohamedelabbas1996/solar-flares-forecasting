5e06da3b83acdaace8527d0282c365fa7d55d4f2
mohamedelabbas1996 <hack1996man@gmail.com>
2024-02-21 06:12:15 -0500

    updated train-job.sh

--- train-job.sh
+++ train-job.sh
@@ -6,6 +6,8 @@
 #SBATCH --time=6:00:00 
 #SBATCH--nodes=2
 export WANDB_MODE=dryrun
+cd /home/melabbas/projects/def-gunes/melabbas/solar-flares-forecasting
+module purge
 module load python/3.10
 source solar-flares-forecasting/bin/activate
 python python train.py --lr 0.001 --batch_size 64 --num_epochs 2
