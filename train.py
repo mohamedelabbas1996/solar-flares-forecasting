f496968e7ae89bc0f1c460e78f7b58c40643d94d
mohamedelabbas1996 <hack1996man@gmail.com>
2024-02-26 04:35:31 -0500

    modified train.py to experiment with FocalBCE loss

--- train.py
+++ train.py
@@ -12,7 +12,7 @@ from datasets.dataset import MagnetogramDataset
 import torch.nn.functional as F
 import datetime
 from tqdm import tqdm
-
+from loss import FocalBCELoss
 
 def train(model, optimizer, train_loader, validation_loader, num_epochs, criterion, device, interactive=False):
     model.train()
@@ -118,7 +118,7 @@ def main(args):
     
 
     # active region based split 
-    data_df = pd.read_csv("data/SHARP/sharp_magnetograms_labelled_balanced_w_magnetogram_exists_contains_nan_nan_removed_rebalanced.csv")
+    data_df = pd.read_csv("data/SHARP/sharp_magnetograms_labelled_balanced_w_magnetogram_exists_contains_nan_nan_removed.csv")
    
     unique_regions = data_df['harp_no'].unique()
 
@@ -146,14 +146,15 @@ def main(args):
     test_df = data_df[test_mask]
 
     # save splitted data
-    train_df.to_csv("data/SHARP/train_df_v2.csv")
-    valid_df.to_csv("data/SHARP/valid_df_v2.csv")
-    test_df.to_csv("data/SHARP/test_df_v2.csv")
+    train_df.to_csv("data/SHARP/train_df_v3.csv")
+    valid_df.to_csv("data/SHARP/valid_df_v3.csv")
+    test_df.to_csv("data/SHARP/test_df_v3.csv")
 
 
 
     model = CNN()
-    criterion = F.binary_cross_entropy_with_logits
+    #criterion = F.binary_cross_entropy_with_logits
+    criterion = FocalBCELoss()
     optimizer = Adam(model.parameters(), lr=args.lr)
 
     config = dict(
@@ -162,13 +163,13 @@ def main(args):
     optimizer_name = "Adam",
     batch_size =  args.batch_size,
     num_epochs = args.num_epochs,
-    loss_function = "F.binary_cross_entropy_with_logits"
+    loss_function = "FocalBCELoss()"
 )
     wandb.config.update(config)
     wandb.config.update({
-        "train_data":"data/SHARP/train_df_v2.csv",
-        "validation_data":"data/SHARP/valid_df_v2.csv",
-        "test_data":"data/SHARP/test_df_v2.csv"
+        "train_data":"data/SHARP/train_df_v3.csv",
+        "validation_data":"data/SHARP/valid_df_v3.csv",
+        "test_data":"data/SHARP/test_df_v3.csv"
     })
     # initilize model, optimizer, loss, datasets, train,test loaders
     train_dataset = MagnetogramDataset(train_df, magnetograms_dir="data/SHARP/sharp_data_all_magnetograms")
