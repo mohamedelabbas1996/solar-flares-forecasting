392cf1a2c88bb318df87560a694a0db220d6f4e5
mohamedelabbas1996 <hack1996man@gmail.com>
2024-03-04 02:26:29 -0500

    added dataset.py

--- datasets/dataset_v1/dataset.py
+++ datasets/dataset_v1/dataset.py
@@ -55,6 +55,17 @@ class MagnetogramDataset(Dataset):
             transform (callable, optional): Optional transform to be applied on a sample.
         """
         self.dataframe = dataframe
+        self.overall_mean = dataframe['mean'].mean()
+    
+        # Compute the average of the variances from the 'variance' column
+        V_avg = dataframe['variance'].mean()
+        
+        # Compute the variance among the means and scale by the number of observations
+        V_means = dataframe['mean'].var() * n
+        
+        # Combine V_avg and V_means to get the overall variance
+        self.overall_variance = V_avg + V_means
+    
         self.magnetograms_dir = magnetograms_dir
 
     def __len__(self):
@@ -70,10 +81,14 @@ class MagnetogramDataset(Dataset):
         
         # Convert magnetogram and label to PyTorch tensors
         magnetogram = torch.from_numpy(magnetogram).float()
+        overall_std = np.sqrt(self.overall_variance)
+    
+        # Normalize the magnetogram
+        normalized_magnetogram = (magnetogram - self.overall_mean) / overall_std
         label = torch.tensor(1).long()  if label=="positive"  else  torch.tensor(0).long()# Assuming label is an integer class label
         
         
-        return magnetogram, label
+        return normalized_magnetogram, label
 
 
 if __name__ == "__main__":
