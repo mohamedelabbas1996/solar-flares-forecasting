467ed6cecddd14a0779e878c71e720dad9218b22
mohamedelabbas1996 <hack1996man@gmail.com>
2024-04-04 22:13:58 -0400

    added compute_mean_variance

---
+++ datasets/sharp_sun_et_al/compute_mean_variance.py
@@ -0,0 +1,19 @@
+import numpy as np
+import pandas as pd
+from tqdm import tqdm
+from dataset import MagnetogramDataset
+
+sharp_df = pd.read_csv("datasets/sharp_sun_et_al/sharp_sun_et_al_filtered.csv")
+ds = MagnetogramDataset(sharp_df, magnetograms_dirs=["data/SHARP/sharp_magnetograms_sun_et_al_decompressed/sharp_magnetograms_sun_et_al_compressed_1","data/SHARP/sharp_data_all_magnetograms"])
+means = []
+vars = []
+for idx, row in tqdm(range(len(ds))):
+    magnetogram = ds[idx][0].numpy()
+    mean = np.mean(magnetogram)
+    var = np.var(magnetogram)
+    means.append(mean)
+    vars.append(var)
+sharp_df["mean"] = pd.Series(means)
+sharp_df["variance"] = pd.Series(vars)
+sharp_df.to_csv("datasets/sharp_sun_et_al/sharp_sun_et_al_filtered_mean_variance.csv")    
+
