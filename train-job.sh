fd66660d3d617e39cc92d3e77e37a4ffb120ca7e
mohamedelabbas1996 <hack1996man@gmail.com>
2024-04-04 22:59:43 -0400

    updated train-job.sh

--- train-job.sh
+++ train-job.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 #SBATCH --account=def-gunes
-#SBATCH --gres=gpu:1 
+##SBATCH --gres=gpu:1 
 #SBATCH --cpus-per-task=4
 #SBATCH --mem=16G 
 #SBATCH --time=2:00:00 
-#SBATCH--nodes=2
+#SBATCH--nodes=1
 export WANDB_MODE=dryrun
 cd /home/melabbas/projects/def-gunes/melabbas/solar-flares-forecasting
 module purge
